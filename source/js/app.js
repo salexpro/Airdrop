6e56281307fe54bd21a8ff01817f766f25e5877e
Alexander <dev@salex.pro>
2018-04-04 02:07:27 +0300

    landing page

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,23 @@
 /* 
-@codekit-prepend '../../node_modules/jquery/dist/jquery.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
+@codekit-prepend '../../node_modules/jquery/dist/jquery.min';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.triggers.min';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.min';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.smoothScroll.min';
 */
 
 $(document).foundation();
+
+$('form[action]').submit(function(e) {
+    e.preventDefault();
+    const action = $(this).attr('action');
+    switch (action) {
+        case 'join':
+            $('#join').foundation('open');
+            break;
+        default:
+            break;
+    }
+});
