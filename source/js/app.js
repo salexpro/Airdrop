832e58d49949671915e1f93168ac1007589cb0b3
Alexander <dev@salex.pro>
2018-04-18 01:50:28 +0300

    many fixes
    - new promo design/styles/typography
    - how section vector step images
    - promo form bouncing animation
    - status popup
    - remove node_modules from codekit config

--- source/js/app.js
+++ source/js/app.js
@@ -1,26 +1,35 @@
 /* 
-@codekit-prepend '../../node_modules/jquery/dist/jquery.min';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.triggers.min';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.min';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.motion.min';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.smoothScroll.min';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.offcanvas.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.reveal.min';
+@codekit-prepend quiet '../../node_modules/jquery/dist/jquery.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.triggers.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.motion.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.smoothScroll.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.offcanvas.min.js';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.reveal.min';
 */
 
 $(document).foundation();
-
+let user_email;
+user_email = $('[type="email"]', this).val();
 $('form[action]').submit(function(e) {
     e.preventDefault();
     const action = $(this).attr('action');
     switch (action) {
         case 'join':
-            $('#join').foundation('open');
+            $('#step2').foundation('open');
+            break;
+        case 'send':
+            $('#success').foundation('open');
             break;
         default:
             break;
     }
 });
+
+// Animations
+setInterval(() => {
+    $('.form--head').toggleClass('bounce');
+}, 3000)
